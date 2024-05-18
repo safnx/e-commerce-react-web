1ca83f89a04dc567528cc1c813d266640542867c
safnx <safvanmohammedn@gmail.com>
2024-05-18 12:53:16 +0530

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+function Header() {
+  return (
+    <header className='header'>
+        <Link to='/' style={{textDecoration:'none',color:'#fff'}}>
+        <h1>Safstore</h1>
+        </Link>
+
+        <Link to='/logout'>
+        <button className='logout-button'>Log out</button>
+        </Link>
+        
+
+
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Logout.css'
+function Logout() {
+  return (
+    <div className='logout-container'>
+        <div className='logount-content'>
+            <h1>
+                You are successfully logged out
+            </h1>
+            <p>Thank you for using our application.Have a great day</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Logout
